d50c3b5df1e3b17fc93bdbe3c9153b9bb7cb6a71
Your Name <you@example.com>
2023-08-01 14:58:45 +0800

    calc avg of reviews

--- read.py
+++ read.py
@@ -3,10 +3,9 @@ count = 0
 with open('reviews.txt', 'r') as x:
 	for line in x:
 		data.append(line)
-		count += 1
-		if count % 1000 == 0: #讓數量每隔1000筆印一次
-			print(len(data))
 
-print(data[0])
-print('-----------') #分隔線
-print(data[1])
+sum_string = 0
+for word in data: #檢視DATA中的每一筆資料
+	sum_string = sum_string + len(word) #輪到下一行評論字數時，跟前面的總和加在一起
+	answer = sum_string/len(data)
+print('留言的平均長度為', answer)
